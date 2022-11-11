e072e2c5d3b3843bfc5ac93e12cc6453c43c8f07
HarshAndroid <rajpurohitharsh2020@gmail.com>
2022-11-11 07:57:39 +0530

    *Home Screen UI Design

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      //app bar
+      appBar: AppBar(
+        leading: const Icon(CupertinoIcons.home),
+        title: const Text('We Chat'),
+        actions: [
+          //search user button
+          IconButton(onPressed: () {}, icon: const Icon(Icons.search)),
+
+          //more features button
+          IconButton(onPressed: () {}, icon: const Icon(Icons.more_vert))
+        ],
+      ),
+
+      //floating button to add new user
+      floatingActionButton: Padding(
+        padding: const EdgeInsets.only(bottom: 10),
+        child: FloatingActionButton(
+            onPressed: () {}, child: const Icon(Icons.add_comment_rounded)),
+      ),
+    );
+  }
+}
