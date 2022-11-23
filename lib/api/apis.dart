bba20d168bea0f927bcb0b16d1c73a32574d9098
HarshAndroid <rajpurohitharsh2020@gmail.com>
2022-11-23 11:50:23 +0530

    *Firestore dependency added.

--- lib/api/apis.dart
+++ lib/api/apis.dart
@@ -1,5 +1,10 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 class APIs {
+  // for authentication
   static FirebaseAuth auth = FirebaseAuth.instance;
+
+  // for accessing cloud firestore database
+  static FirebaseFirestore firestore = FirebaseFirestore.instance;
 }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,10 +5,12 @@
 import FlutterMacOS
 import Foundation
 
+import cloud_firestore
 import firebase_auth
 import firebase_core
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
 }
