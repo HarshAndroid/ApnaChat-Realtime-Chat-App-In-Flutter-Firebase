b8d40c93f5b597dce7c95e236d8d75bb1ced3225
HarshAndroid <rajpurohitharsh2020@gmail.com>
2022-11-29 13:37:56 +0530

    *Profile Picture In Chat User Card

--- lib/widgets/chat_user_card.dart
+++ lib/widgets/chat_user_card.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -26,7 +27,17 @@ class _ChatUserCardState extends State<ChatUserCard> {
         onTap: () {},
         child: ListTile(
           //user profile picture
-          leading: const CircleAvatar(child: Icon(CupertinoIcons.person)),
+          // leading: const CircleAvatar(child: Icon(CupertinoIcons.person)),
+          leading: ClipRRect(
+            borderRadius: BorderRadius.circular(mq.height * .3),
+            child: CachedNetworkImage(
+              width: mq.height * .055,
+              height: mq.height * .055,
+              imageUrl: widget.user.image,
+              errorWidget: (context, url, error) =>
+                  const CircleAvatar(child: Icon(CupertinoIcons.person)),
+            ),
+          ),
 
           //user name
           title: Text(widget.user.name),
@@ -35,10 +46,17 @@ class _ChatUserCardState extends State<ChatUserCard> {
           subtitle: Text(widget.user.about, maxLines: 1),
 
           //last message time
-          trailing: const Text(
-            '12:00 PM',
-            style: TextStyle(color: Colors.black54),
+          trailing: Container(
+            width: 15,
+            height: 15,
+            decoration: BoxDecoration(
+                color: Colors.greenAccent.shade400,
+                borderRadius: BorderRadius.circular(10)),
           ),
+          // trailing: const Text(
+          //   '12:00 PM',
+          //   style: TextStyle(color: Colors.black54),
+          // ),
         ),
       ),
     );
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -8,9 +8,13 @@ import Foundation
 import cloud_firestore
 import firebase_auth
 import firebase_core
+import path_provider_macos
+import sqflite
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
 }
