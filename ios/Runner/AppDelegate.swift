61cbaf8904e7544e3dff5459893d46eff39f1b40
HarshAndroid <rajpurohitharsh2020@gmail.com>
2024-12-19 22:31:03 +0530

    *Project Upgrades.

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,14 +1,7 @@
-<<<<<<< HEAD
 import Flutter
 import UIKit
 
 @main
-=======
-import UIKit
-import Flutter
-
-@UIApplicationMain
->>>>>>> 806dca544752c5c978f39977aeb4005785d5f0ee
 @objc class AppDelegate: FlutterAppDelegate {
   override func application(
     _ application: UIApplication,
